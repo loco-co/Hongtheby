abbc37962c1fa67c7f471e7ccd28e5d4d393f533
loco-co <loco2925@gmail.com>
2021-10-23 19:24:27 +0900

    Chapter 2.2

--- art/admin.py
+++ art/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ['subject']  # 제목으로 검색하는 관리자 기능 추가
+
+admin.site.register(Item)  # 관리자 사이트에 모델 등록
