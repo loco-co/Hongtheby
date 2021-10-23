702f984606faaac89234ab6885379d86702200c5
loco-co <loco2925@gmail.com>
2021-10-23 19:58:00 +0900

    Chapter 2.5

--- art/urls.py
+++ art/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'art'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:item_id>/', views.detail),  # 매핑 룰에 의해 /art/item_id/가 적용되고 views.detail 함수가 실행됨
+    path('', views.index, name='index'),  # url별칭 사용
+    path('<int:item_id>/', views.detail, name='detail'),  # 매핑 룰에 의해 /art/item_id/가 적용되고 views.detail 함수가 실행됨
 ]
