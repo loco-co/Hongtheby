e6d85876a9f9f2543a2e7a6e29925b2ed3eeb30c
loco-co <loco2925@gmail.com>
2021-10-23 19:14:16 +0900

    Chapter 2.2

--- art/models.py
+++ art/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    subject = models.CharField(max_length=200)
+    title = models.CharField(max_length=20)
+    price = models.IntegerField()
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    image = models.ImageField()
+
+    def __str__(self):  # id 대신 제목을 반환하도록
+        return self.title
--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'art.apps.ArtConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
