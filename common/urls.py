536a5e8a3000799b442352611d582cacf9b0dd16
loco-co <loco2925@gmail.com>
2021-10-24 17:18:32 +0900

    Account

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from . import views
 
 app_name = 'common'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'),
          name='login'),  # loginview 그대로 사용, registration 이 아닌 common 디렉터리에 템플릿을 생성
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
